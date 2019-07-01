b7a2c36e438c61fb9217955c98a6f423b30301dd
LogeshDeveloper <logeshdeveloper08@gmail.com>
2019-07-01 11:50:55 +0530

    added dir properly

---
+++ dist/Portfolio/assets/Contact-form.php
@@ -0,0 +1,17 @@
+<?php 
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'logeshdeveloper08@gmail.com';
+    $email_subject = "Contact Form"
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n". 
+                      "User Message": $message.\n"; 
+                      
+    $to = "logesh1999@gmail.com"
+    $headers = "From: $email_from \r\n";
+    $headers = "Reply-To: $visitor_email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: app.component.html");
